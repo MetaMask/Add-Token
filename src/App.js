f89d53312e4a06cf8da359dcbc55807f2c0aed7d
Dan Finlay <dan@danfinlay.com>
2018-08-28 17:30:18 -0700

    Add github banner

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   render(props, context) {
     return (
       <div className="App">
+        <a href="https://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
         <Web3Provider>
           <Route path="/edit" component={EditTokenPanel} />
           <Route path="/" component={AddTokenPanel} />
