461f7005a2d8cf657b555c0bab54f3c8a3b56fd8
Austin Akers <austin.akers5@gmail.com>
2021-09-09 08:12:30 -0700

    [FIX] Refactored App and removed unused packages (#39)
    * refactored App.js into a functional component and removed web3-react
    
    * removed dead code
    
    * removed EthereumProvider file
    
    * added removed web3 use
    
    * refactored switch network notice
    
    * removed dead/unused packages
    
    * removed pseudo code
    
    * added prop-types
    
    * added use effect to detect if provider is loaded
    
    * fixed loading issue and useEffect rendering
    
    * added centering for the entire app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,65 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Web3Provider } from 'react-web3';
+import detectEthereumProvider from '@metamask/detect-provider';
+import React, { useState } from 'react';
+import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom';
 import AddTokenPanel from './AddTokenPanel';
-import EditTokenPanel from './EditTokenPanel';
+import './App.css';
 import DownloadMetaMaskButton from './DownloadMetaMaskButton';
-import { BrowserRouter, Route, Switch, HashRouter } from 'react-router-dom';
+import EditTokenPanel from './EditTokenPanel';
+import loadingSvg from './loading.svg';
 
-class App extends Component {
 
-  render(props, context) {
-    return (
-      <div className="App">
-        <a className="github-banner" href="https://github.com/MetaMask/Add-Token"><img src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub" /></a>
-        <Web3Provider
-          web3UnavailableScreen={() => <div>
-            <p>You need a web3 browser like MetaMask to use this site and manage cryptocurrencies.</p>
-            <DownloadMetaMaskButton/>
-          </div>}
-        >
-          <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <HashRouter hashType="noslash">
-              <Switch>
-                <Route path="/edit" component={EditTokenPanel} />
-                <Route path="/add" component={AddTokenPanel} />
-                <Route path="/" component={AddTokenPanel} />
-              </Switch>
-            </HashRouter>
-          </BrowserRouter>
-        </Web3Provider>
+const MainContent = () => {
+  return (
+    <div>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter hashType="noslash">
+          <Switch>
+            <Route path="/edit" component={EditTokenPanel} />
+            <Route path="/add" component={AddTokenPanel} />
+            <Route path="/" component={AddTokenPanel} />
+          </Switch>
+        </HashRouter>
+      </BrowserRouter>
+    </div>
+  )
+};
+
+const ErrorContent = () => {
+  return (
+    <div>
+      <div>
+        <h2>You need a web3 browser like MetaMask to use this site and manage cryptocurrencies.</h2>
+        <DownloadMetaMaskButton />
       </div>
-    );
-  }
+    </div>
+  )
 }
+
+const App = () => {
+  const [isProviderLoaded, setProvider] = useState([]);
+  const [isLoading, setLoading] = useState([true]);
+
+  const checkEthereumProvider = async () => {
+    const provider = await detectEthereumProvider()
+    setLoading(false)
+    setProvider(provider)
+  }
+
+  checkEthereumProvider()
+
+  return (
+    <div className="App">
+      {isLoading ? <div><div><img className="loading-spinner" src={loadingSvg} /><h2>Loading.....</h2></div></div>
+        : <div>
+          <a className="github-banner" href="https://github.com/MetaMask/Add-Token"><img src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub" /></a>
+
+          {isProviderLoaded ? <MainContent /> : <ErrorContent />}
+
+        </div>}
+    </div>
+  );
+}
+
+
 
 export default App;
